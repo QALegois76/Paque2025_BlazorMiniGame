5553a239867c750e054ee5b022c73d798dd9440b
Antoine LEGOIS <antoine.legois@altimet.fr>
2025-04-19 15:43:19 +0200

    Add project files.

---
+++ js/simon.js
@@ -0,0 +1,10 @@
+﻿window.playSimonSound = function (index) {
+    const sounds = [
+        '/res/sounds/btn1.mp3',
+        '/res/sounds/btn2.mp3',
+        '/res/sounds/btn3.mp3',
+        '/res/sounds/btn4.mp3'
+    ];
+    const audio = new Audio(sounds[index]);
+    audio.play();
+};
